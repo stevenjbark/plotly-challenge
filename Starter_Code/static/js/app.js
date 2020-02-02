d4adb409691cc8c8a0329c93f2b3fbd3a6870cf8
Steven J Bark <stevenjbark@gmail.com>
2020-02-02 14:43:07 -0600

    Fifth Commit: Bar and Bubble Charts, DropDown Menu, Demographics All Working

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -85,18 +85,75 @@ d3.json("samples.json").then(function(data) {
         array = longName.split(";");
         q = array.slice(array.length-1, array.length);
         bacteriaSpecies.push(q[0])
-    })
+    });
+
     console.log(bacteriaSpecies);
     console.log(bacteriaFullName);
     
     
+
+    //INTEGRATE RETRIEVING DEMOGRAPHIC INFORMATION WITH UPDATEPLOTLY FUNCTION ON SAMPLEID
+    //Extract metadata array from json file
+    d3.json("samples.json").then(function(demogData) {
+        var metadata = demogData.metadata;
+        //console.log(metadata);
+
+        //Filter metadata based on sampleID to obtain demographic data for sample
+        var demographicData = metadata.filter(data => data.id == sampleID);
+
+        //Extract age, bbtype, ethnicity, gender, location, wfreq data
+        var age = demographicData[0].age;
+        var ethnicity = demographicData[0].ethnicity;
+        var gender = demographicData[0].gender;
+        var location = demographicData[0].location;
+        var wfreq = demographicData[0].wfreq;
+
+        
+        //Clear previous data in demographicMenu: Select in Line 1, Reassign to nothing in Line2
+        var oldDemographicMenu = d3.select("#sample-metadata");
+        oldDemographicMenu.html("");
+        
+        
+        //Select sample-metadata id using d3. This is where I will insert text for demographicData
+        var demographicMenu = d3.select("#sample-metadata");
+
+            var cell = demographicMenu.append("p");
+            cell.text(`Age: ${age}`);
+                
+            var cell = demographicMenu.append("p");
+            cell.text(`Ethnicity: ${ethnicity}`);
+
+            var cell = demographicMenu.append("p");
+            cell.text(`Gender: ${gender}`);
+                
+            var cell = demographicMenu.append("p");
+            cell.text(`Location: ${location}`);
+
+            var cell = demographicMenu.append("p");
+            cell.text(`Wash Frequency: ${wfreq}`);
+                
+        });
     
+    
+    console.log(ids);
     //PLOTTING BAR CHART IN PLOTLY
     //Create trace
     var trace = {
         x: ids,
         y: quantity,
         type: "bar",
+        marker: {
+            color: [
+            '#1f77b4',  // muted blue
+            '#ff7f0e',  // safety orange
+            '#2ca02c',  // cooked asparagus green
+            '#d62728',  // brick red
+            '#9467bd',  // muted purple
+            '#8c564b',  // chestnut brown
+            '#e377c2',  // raspberry yogurt pink
+            '#7f7f7f',  // middle gray
+            '#bcbd22',  // curry yellow-green
+            ]},
         text: bacteriaSpecies
     };
 
@@ -105,21 +162,54 @@ d3.json("samples.json").then(function(data) {
 
     //Define plot layout
     var layout = {
+        height: 600,
+        width: 900,
         title: `Quantitation of Bacterial Species in Sample ${sampleID}`,
         xaxis: {title: "ID"},
-        yaxis: {title: "Sample Quantitation"}
+        yaxis: {title: "Bacterial Amount"}
     };
 
-    //Plot chart using the #bar ID
+    //Plot chart using the "bar" ID
     Plotly.newPlot("bar", data, layout);
 
 
     //PLOTTING BUBBLE CHART IN PLOTLY
+    //Create trace
+    var trace2 = {
+        x: ids,
+        y: quantity,
+        mode: "markers",
+        marker: {
+            size: quantity,
+            color: [
+            '#1f77b4',  // muted blue
+            '#ff7f0e',  // safety orange
+            '#2ca02c',  // cooked asparagus green
+            '#d62728',  // brick red
+            '#9467bd',  // muted purple
+            '#8c564b',  // chestnut brown
+            '#e377c2',  // raspberry yogurt pink
+            '#7f7f7f',  // middle gray
+            '#bcbd22',  // curry yellow-green
+            ]
+            },
+        text: bacteriaSpecies
+    };
 
+    //Create data array
+    var data2 = [trace2];
 
+    //Create layout
+    var layout2 = {
+        title: `Quantitation of Bacterial Species in Sample ${sampleID}`,
+        xaxis: {title: "ID"},
+        yaxis: {title: "Bacterial Amount"},
 
-    
+    };
 
-});
+    //Plot bubble chart using "bubble" id
+    Plotly.newPlot("bubble", data2, layout2)
 
+    });
 };
+
